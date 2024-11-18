f4b1f57b66429e6b385f6a35b485d38eddd83877
Pepe Fabra Valverde <52806937+jofaval@users.noreply.github.com>
2024-11-18 16:51:03 +0100

    feat(security): check commit and config

--- git/hooks/pre-commit.sh
+++ git/hooks/pre-commit.sh
@@ -1,6 +1,7 @@
-authors="$(git show -s --format='%ae' HEAD)"
+config_author="$(git config user.email)"
+config_comitter=$(git var GIT_AUTHOR_IDENT)
 
-if [[ "$authors" =~ "capgemini.com" ]]; then
+if [[ "$config_author" =~ "capgemini.com"  ||  "$config_comitter" =~ "capgemini.com" ]]; then
   echo "¡Alto ahí gañán! Las cuentas corporativas no están permitidas."
   exit 1
 fi
