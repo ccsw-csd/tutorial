e1558ca5b566814e646bf8e65960b9ddbabea7c7
Pepe Fabra Valverde <52806937+jofaval@users.noreply.github.com>
2024-11-18 17:16:53 +0100

    fix(security): remove dot

--- git/hooks/install.bat
+++ git/hooks/install.bat
@@ -1,2 +1,2 @@
 FOR /F "usebackq delims=" %%G IN (`git rev-parse --show-toplevel`) DO SET "GIT_PATH=%%G"
-COPY "./pre-commit.sh" "%GIT_PATH%/hooks/pre-commit"
+COPY "pre-commit.sh" "%GIT_PATH%/hooks/pre-commit"
